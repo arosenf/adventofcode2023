5dc8abe304a9ae23cc8b107b142e2a551b864cd8
Angelo Rosenfelder <angelo@rosenfelder.ch>
2023-12-01 21:01:47 +0100

    day 1 solution 1

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,7 +1,36 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import kotlin.system.exitProcess
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    if (args.isEmpty()) {
+        usage("?")
+        exitProcess(1)
+    }
+
+    val calibrationDocument = args.first()
+    println("Recovering $calibrationDocument")
+
+    val calibrationValue = parseCalibrationDocument(calibrationDocument)
+    println("Calibration value: $calibrationValue")
+}
+
+fun parseCalibrationDocument(calibrationDocument: String): Int {
+    val resource = {}::class.java.getResourceAsStream(calibrationDocument)?.bufferedReader()
+    if (resource == null) {
+        usage(calibrationDocument)
+        exitProcess(1)
+    }
+
+    return resource.lines()
+        .map(::extractCalibrationValue)
+        .reduce { x, y -> x + y }
+        .orElse(0)
+}
+
+fun extractCalibrationValue(calibrationLine: String): Int {
+    val (digit, _) = calibrationLine.partition { c -> c.isDigit() }
+    return "${digit.first()}${digit.last()}".toInt()
+}
+
+fun usage(doc: String) {
+    println("Calibration document $doc not found")
 }
